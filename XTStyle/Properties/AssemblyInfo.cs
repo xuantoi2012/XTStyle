ecd214b4fcd21c53d10f71d72fbdd3d98f7ea899
Trần Xuân Tới <conduonguocmo268@gmail.com>
2025-11-19 07:38:40 +0700

    Add project files.

---
+++ XTStyle/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Windows;
+using System.Windows.Markup;
+
+// Cho phép XAML tìm Generic.xaml trong thư mục Themes
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, // theme-specific resources
+    ResourceDictionaryLocation.SourceAssembly // Generic.xaml location
+)]
+
+// Thông tin Assembly
+[assembly: AssemblyTitle("XTStyle")]
+[assembly: AssemblyDescription("Modern WPF Style Library")]
+[assembly: AssemblyCompany("Your Company")]
+[assembly: AssemblyProduct("XTStyle")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Cho phép XAML Namespace
+[assembly: XmlnsPrefix("http://schemas.xtstyle.com/wpf", "xt")]
+[assembly: XmlnsDefinition("http://schemas.xtstyle.com/wpf", "XTStyle")]
+[assembly: XmlnsDefinition("http://schemas.xtstyle.com/wpf", "XTStyle.Controls")]
